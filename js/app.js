dd060bcd08fe18211a130e830d2ae52cb1178c17
kankkuna <karina842@hotmail.com>
2018-01-08 17:55:55 -0500

    se avanza login con materialize

---
+++ js/app.js
@@ -0,0 +1,9 @@
+/*$('.carousel.carousel-slider').carousel({fullWidth: true});
+$(document).ready(function(){
+    // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+    $('.modal').modal();
+  });*/
+
+  $(document).ready(function(){
+        $('.slider').slider();
+      });
