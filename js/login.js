bff02f0dffca607fcdaf84042fe255270fbe4037
kankkuna <karina842@hotmail.com>
2018-01-09 23:17:00 -0500

    incorporación de modal y botones de publicación de texto e imágenes

--- js/login.js
+++ js/login.js
@@ -3,3 +3,11 @@ $(document).ready(function() {
   $('.slider').slider();
   $('.modal').modal();
 });
+
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  $('g-signin2').css('display', 'none');
+  $('data').css('display', 'block');
+  $('#pic').attr('src', profile, getImageUrl());
+  $('#email').text(profile.getEmail());
+}
