9b427f8fe924f19d1c6f5b7621ab6c0b2b7774d3
Rocciescobar <roccisen.88@gmail.com>
2018-01-10 03:24:50 -0500

    Obtener data del usuario, colocarlo en home.html, redireccionar vistas y funcionalidad de cerrar cesión

--- js/home.js
+++ js/home.js
@@ -3,6 +3,56 @@ $(document).ready(function() {
   $('.button-collapse').sideNav();
   $('.modal').modal();
   $('.fixed-action-btn').closeFAB();
-  // Initialize collapsible (uncomment the line below if you use the dropdown variation)
-  // $('.collapsible').collapsible();
+
+  // Initialize Firebase
+  var config = {
+    apiKey: 'AIzaSyAdfKIfUVywLMX1SkbPAu-T4naz2GMTSro',
+    authDomain: 'teachandlearn-d4eb8.firebaseapp.com',
+    databaseURL: 'https://teachandlearn-d4eb8.firebaseio.com',
+    projectId: 'teachandlearn-d4eb8',
+    storageBucket: '',
+    messagingSenderId: '13064064731'
+  };
+  firebase.initializeApp(config);
+
+  // Obteniendo información del usuario
+  var nameHome = $('#nameHome');
+  var nameProfile = $('#nameProfile');
+  var mailHome = $('#mailHome');
+  var imgUser = $('#imgUser');
+  var imgUserProfile = $('#imgUserProfile');
+
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+      var displayName = user.displayName;
+      var email = user.email;
+      var emailVerified = user.emailVerified;
+      var photoURL = user.photoURL;
+      var isAnonymous = user.isAnonymous;
+      var uid = user.uid;
+      var providerData = user.providerData;
+      // ...
+      nameHome.text(displayName);
+      nameProfile.text(displayName);      
+      mailHome.text(email);
+      imgUser.attr('src', photoURL);
+      imgUserProfile.attr('src', photoURL);
+    } else {
+      // User is signed out.
+      // ...
+    }
+  });
+
+  // Cerrar sesión
+  var btnLogout = $('#btnLogout');
+
+  btnLogout.click(function() {
+    firebase.auth().signOut().then(function() {
+      // Sign-out successful.
+      window.location.href = '../index.html';
+    }).catch(function(error) {
+      // An error happened.
+    });
+  });
 });
--- js/login.js
+++ js/login.js
@@ -48,32 +48,13 @@ $(document).ready(function() {
     });
   });
 
-  // function observer() {
-  // firebase.auth().onAuthStateChanged(function(user) {
-  // if (user) {
-  // User is signed in.
-  // var displayName = user.displayName;
-  // var email = user.email;
-  // var emailVerified = user.emailVerified;
-  // var photoURL = user.photoURL;
-  // var isAnonymous = user.isAnonymous;
-  // var uid = user.uid;
-  // var providerData = user.providerData;        
-  // } else {
-  // User is signed out.
-  // ...
-  // }
-  // });    
-  // };  
-  // observer();
-
   // Login con google
   var providerGoogle = new firebase.auth.GoogleAuthProvider();
   $('#login-gmail').click(function() {
     firebase.auth().signInWithPopup(providerGoogle).then(function(result) {
       saveData(result.user);
       console.log(result.user);
-      window.location.href = '../views/home.html';
+      // window.location.href = '../views/home.html';
     });
   });
 
@@ -83,18 +64,20 @@ $(document).ready(function() {
     firebase.auth().signInWithPopup(providerFaceb).then(function(result) {
       saveData(result.user);
       console.log(result.user);
-      window.location.href = '../views/home.html';
+      // window.location.href = '../views/home.html';
     });
   });
   
   // Función para guardar datos de los usuarios
   function saveData(user) {
-    var user = {
+    var users = {
       uid: user.uid,
       nombre: user.displayName,
       email: user.email,
       foto: user.photoURL
     };
-    firebase.database().ref('datapp').push(user);
+    firebase.database().ref('datapp').push(users).then(user => {
+      window.location.href = '../views/home.html';
+    });
   };
 });
