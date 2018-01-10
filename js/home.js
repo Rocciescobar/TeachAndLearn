77dee186b637f97ac0f045942750a2906d434326
kankkuna <karina842@hotmail.com>
2018-01-09 23:48:07 -0500

    incorporación de modal y botones de publicación de texto e imágenes

--- js/home.js
+++ js/home.js
@@ -6,3 +6,89 @@ $(document).ready(function() {
   // Initialize collapsible (uncomment the line below if you use the dropdown variation)
   // $('.collapsible').collapsible();
 });
+/*<div class="textHeader">
+        <h1>Twitter</h1>
+      </div>
+    </header>
+    <section>
+      <div id="container">
+        <div class="box-info">
+          <h2>Reto de Código</h2>
+          <p>Hola, <br> soy front-end developer jr.</p>
+          <hr>
+          <p>CÓDIGO</p>
+        </div>
+        <div class="box-form">
+          <form>
+            <textarea type="text" id="catch-post" rows="3" placeholder="¿Qué estás pensando?"></textarea>
+          </form>
+          <div class="button-counter">
+            <button type="submit" id="save-post" class="btn">TWEET</button>
+            <p class="counter">140</p>
+          </div>
+        </div>
+      </div>
+      <div id="container-posts">
+      </div>
+    </section>*/
+window.addEventListener('load', function() {
+  var catchPost = document.getElementById('catch-post');
+  var button = document.getElementById('save-post');
+  var containerPosts = document.getElementById('container-posts');
+  var counter = document.querySelector('.counter');
+
+  // Creando contador
+  catchPost.addEventListener('keyup', function() {
+    var postLength = catchPost.value.length;
+    var characters = 140 - postLength;
+    counter.textContent = characters;
+
+    if (postLength < 120) {
+      counter.className = 'text-color';
+      button.disabled = false;
+    } else if (postLength > 120 && postLength < 130) {
+      counter.className = 'text-color-120';
+      button.disabled = false;
+    } else if (postLength > 130 && postLength < 140) {
+      counter.className = 'text-color-130';
+      button.disabled = false;
+    } else if (postLength > 140) {
+      counter.className = 'text-color';
+      button.disabled = true;
+    }
+  });
+
+  // Creando posts
+  button.addEventListener('click', function(event) {
+    for (var i = 0; i < catchPost.value.length; i++) {
+      if (catchPost.value[i] === ' ') {
+        event.preventDefault();
+      } else {
+        var post = document.createElement('p');
+        post.textContent = catchPost.value;
+
+        // Agregando formato  de hora
+        post.innerHTML = catchPost.value + '<br>' + moment().format('hh:mm a');
+
+        post.classList.add('posts');
+
+        containerPosts.insertBefore(post, containerPosts.firstElementChild);
+
+        catchPost.value = '';
+        catchPost.focus();
+        counter.textContent = 140;
+        counter.className = 'text-color';
+        catchPost.style.height = '66px';
+      }
+    }
+  });
+
+  // Textarea ajustable de acuerdo al tamaño del texto
+  catchPost.addEventListener('keydown', function() {
+    var keyboardEnter = this;
+    setTimeout(function() {
+      keyboardEnter.style.height = 'auto';
+      keyboardEnter.style.height = keyboardEnter.scrollHeight + 'px';
+    }, 0);
+  });
+});
