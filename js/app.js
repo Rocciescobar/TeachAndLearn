25de20d9ea29c83049d909b7339c9866846ec5bc
kankkuna <karina842@hotmail.com>
2018-01-12 19:38:50 -0500

    se crea segundo modal en pestaña mensajes

--- js/app.js
+++ js/app.js
@@ -1,8 +1,16 @@
 $(document).ready(function() {
   // Initialize collapse button
   $('.button-collapse').sideNav();
-  $('.modal').modal();
+  $('.modal').modal({
+    ready: function(modal, trigger) { // Callback for Modal open. Modal and trigger parameters available.
+      console.log(modal, trigger);
+    },
+  }
+  );
   $('.fixed-action-btn').closeFAB();
+  $('#modal2').modal({
+
+  });
 
   //  Efecto loader en notificaciones y mensajes
   setTimeout(function() {
@@ -52,16 +60,16 @@ $(document).ready(function() {
       var uid = user.uid;
       var providerData = user.providerData;
       // ...
-      nameHome.text(displayName);     
+      nameHome.text(displayName);
       mailHome.text(email);
       imgUser.attr('src', photoURL);
 
-      // Creando posts 
+      // Creando posts
       buttonPost.click(function() {
         var catchPost = $('#textarea1').val();
         containerPosts.prepend('<div class="col s12"><div class="card blue-grey darken-1"><div class="card-content white-text"><div class="row valign-wrapper"><div class="col s2"><img src="' + photoURL + '" alt="" class="circle responsive-img"></div><div class="col s10"><span class=" light-green-text"><strong id="namePosts">' + displayName + '</strong></span><p class="gray-text">' + moment().format('MMM Do YY') + ' a las ' + moment().format('hh:mm a') + '</p></div></div><p>' + catchPost + '</p></div><div class="card-action"><a href="#"><i class="material-icons  light-green-text">favorite_border</i></a><a href="#"><i class="material-icons  light-green-text">comment</i></a><a href="#"><i class="material-icons light-green-text">share</i></a></div></div></div>');
         $('#textarea1').val('');
-      });      
+      });
     } else {
       // User is signed out.
       // ...
